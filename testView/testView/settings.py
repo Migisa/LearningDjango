96cb38240ff2914e4e34ecefff88363543c94f0e
unknown <Aleksandra.Iljina@LONMDS-LTEYGM61.ad.insidemedia.net>
2013-09-12 15:22:48 +0100

    Learned Django templates and inheritance

--- testView/testView/settings.py
+++ testView/testView/settings.py
@@ -1,4 +1,6 @@
 # Django settings for testView project.
+import os.path
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -111,6 +113,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+	#"C:\\Users\\Aleksandra.Iljina\\Documents\\Projects\\SocialListingWebTool\\testView\\testView\\templates",
+	os.path.join(os.path.dirname(__file__), 'templates').replace("\\", "/"),
 )
 
 INSTALLED_APPS = (
--- testView/testView/views.py
+++ testView/testView/views.py
@@ -1,23 +1,42 @@
 from django.http import HttpResponse, Http404
+#from django.template import Template, Context
+#from django.template.loader import get_template
 import datetime
-
+from django.shortcuts import render
 
 def hello(request):
 	return HttpResponse("Hello World")
 	
 	
+#def current_datetime(request):
+	#now = datetime.datetime.now()
+	#html = "<html><body> It is now %s.</body></html>" % now
+	#t = Template("<html><body> It is now {{ current_date }}. </body></html>")
+	#t = get_template('current_datetime.html')
+	#html = t.render(Context({'current_date': now}))
+	#return HttpResponse(html)
+	
+	
 def current_datetime(request):
 	now = datetime.datetime.now()
-	html = "<html><body> It is now %s.</body></html>" % now
-	return HttpResponse(html)
+	return render(request, 'current_datetime.html', {'current_date': now})
 	
+	
+#def hours_ahead(request, offset):
+#	try:
+#		Offset = int(offset)
+#	except ValueError:
+#		raise Http404()
+		
+#	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+#	html = "<html><body> In %s hour(s), it will be %s.</body></html>" % (offset, dt)
+#	return HttpResponse(html)
 	
 def hours_ahead(request, offset):
 	try:
-		Offset = int(offset)
+		offset = int(offset)
 	except ValueError:
 		raise Http404()
 		
 	dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
-	html = "<html><body> In %s hour(s), it will be %s.</body></html>" % (offset, dt)
-	return HttpResponse(html)
+	return render(request, 'hours_ahead.html', {'hour_offset': offset, 'next_time': dt})
