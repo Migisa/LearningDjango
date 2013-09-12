20b2ed3e3e610dd0593136fa4d1da2b36b2dbfad
unknown <Aleksandra.Iljina@LONMDS-LTEYGM61.ad.insidemedia.net>
2013-09-12 11:02:37 +0100

    Added testing files

---
+++ mysite/polls/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+
+# Create your models here.
+### creates two models: Poll and Choice
+
+
+class Poll(models.Model):
+	#question = models.CharField(max_length = 200)
+	#pub_data = models.DateTimeField("date published")
+	
+	#def __unicode__(self):
+	#	return self.question
+	print("These are your classes as you start building real apps")
+	
+	
+class Choice(models.Model):
+	poll = models.ForeignKey(Poll)
+	choice_text = models.CharField(max_length = 200)
+	votes = models.IntegerField(default = 0)
+	
+	def __unicode__(self):
+		return self.choice_text
+		
+		
